f077add2933b0dad9fa614422c008ad9255ac0df
runzx <843476168@qq.com>
2017-06-03 19:46:52 +0800

    1. 命令行创建控制器： php artisan  make:controller ZxTest2  2. class ZxTest extends Controller

---
+++ app/Http/Controllers/ZxTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaixiang
+ * Date: 2017/6/3
+ * Time: 19:39
+ */
+
+namespace Http\Controllers;
+
+
+use App\Http\Controllers\Controller;
+
+class ZxTest extends Controller
+{
+
+}
